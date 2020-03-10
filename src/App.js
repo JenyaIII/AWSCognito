3c047146504dc5387b46c276e628a8ab10d1a1c3
JenyaIII <161russ@gmail.com>
2020-03-10 13:33:40 +0300

    add submit button with awsCognition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const poolData = {
+    UserPoolId: 'us-east-2_n3M3PlkvS',
+    ClientId: '22sb38t71saqnb4bk9ouqju9rj'
+  };
+
+  const userPool = new CognitoUserPool(poolData);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    userPool.signUp(email, password, [], null, (err, data) => {
+      if (err) console.error(err);
+      console.log(data);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button type="submit">Sign up</button>
+      </form>
     </div>
   );
 }
